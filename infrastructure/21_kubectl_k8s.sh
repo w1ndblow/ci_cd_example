dfe62d4083eb62f41ba7cef34cf5f90366c2f779
Aleksey Kuznetsov <koyotne@bk.ru>
2026-07-23 18:58:14 +0300

    try deploy to minikube

--- infrastructure/21_kubectl_k8s.sh
+++ infrastructure/21_kubectl_k8s.sh
@@ -4,7 +4,7 @@ source common.sh
 
 run_command "tree k8s_gitlab"
 
-echo "Введите gitlab token:"
+echo "Введите gitlab runner token:"
 read -s gitlab_token
 
 run_command "kubectl create secret -n default generic 'gitlab-runner-secret' --from-literal runner-token=$gitlab_token --from-literal runner-registration-token=''" "ansible-k8s"
